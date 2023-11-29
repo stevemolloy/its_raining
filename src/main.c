f588b1ff31d49b37e0dcb65b9160bcae028c4716
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-11-29 10:21:31 +0100

    QandA structure properly created

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <raylib.h>
+#include <string.h>
 
 #define WIDTH 800
 #define HEIGHT 600
@@ -55,20 +57,66 @@ void free_qanda(QandA *qanda) {
   free(qanda->answers);
 }
 
+int parse_string_to_qanda(QandA *qanda, char *text) {
+  char *cursor = text;
+  while (strncmp("Q:", cursor, 2) !=0 ) cursor++;
+    // cursor now points at "Q:".  Advance it by two places and then search for "A:"
+
+    while (1) {
+    cursor += 2;
+    while (*cursor==' ') cursor++;
+    char *question = cursor;
+
+    while (strncmp("A:", cursor, 2) !=0 ) cursor++;
+    // cursor now points at the "A:" following the question
+
+    size_t q_len = cursor-question;
+    char *q = calloc(q_len, sizeof(char));
+    memcpy(q, question, q_len);
+    q[q_len-1] = '\0';
+    printf("Question: %s\n", q);
+
+    cursor += 2;
+    while (*cursor==' ') cursor++;
+    char *answer = cursor;
+    while (*cursor != '\0' && strncmp("Q:", cursor, 2) !=0 ) cursor++;
+    // cursor now points at the "Q:" following the answer or the null terminator
+    
+    size_t a_len = cursor - answer;
+    char *a = calloc(a_len, sizeof(char));
+    memcpy(a, answer, a_len);
+    a[a_len-1] = '\0';
+    printf("Answer: %s\n", a);
+
+    append_to_qanda(qanda, q, a);
+
+    if (*cursor=='\0') break;
+  }
+
+  return 1;
+}
+
 int main(void) {
   const char* catechism_file = "pretend_catechism.txt";
   char *file_contents = LoadFileText(catechism_file);
-
-  InitWindow(WIDTH, HEIGHT, "It's raining...");
-  SetWindowState(FLAG_WINDOW_RESIZABLE);
-  SetTargetFPS(60);
-
-  while (!WindowShouldClose()) {
-
-    BeginDrawing();
-    ClearBackground(WHITE);
-    EndDrawing();
+  if (strlen(file_contents)<7) {
+    fprintf(stderr, "File %s does not use the correct format\n", catechism_file);
+    exit(1);
   }
+
+  QandA qanda = empty_qanda();
+  parse_string_to_qanda(&qanda, file_contents);
+
+  // InitWindow(WIDTH, HEIGHT, "It's raining...");
+  // SetWindowState(FLAG_WINDOW_RESIZABLE);
+  // SetTargetFPS(60);
+  //
+  // while (!WindowShouldClose()) {
+  //
+  //   BeginDrawing();
+  //   ClearBackground(WHITE);
+  //   EndDrawing();
+  // }
 
   UnloadFileText(file_contents);
 
