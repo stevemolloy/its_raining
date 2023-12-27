64b4ccc2b683b2e1562bbb507bf670503cc9c35f
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-12-27 15:35:01 +0100

    Move convenience functions into the library file

--- src/main.c
+++ src/main.c
@@ -30,34 +30,6 @@
 #define BACK_BTN_TEXT  "Back"
 #define RESET_BTN_TEXT "Reset"
 
-int *CodepointRemoveDuplicates(int *codepoints, int codepointCount, int *codepointsResultCount)
-{
-    int codepointsNoDupsCount = codepointCount;
-    int *codepointsNoDups = (int *)calloc(codepointCount, sizeof(int));
-    memcpy(codepointsNoDups, codepoints, codepointCount*sizeof(int));
-
-    // Remove duplicates
-    for (int i = 0; i < codepointsNoDupsCount; i++)
-    {
-        for (int j = i + 1; j < codepointsNoDupsCount; j++)
-        {
-            if (codepointsNoDups[i] == codepointsNoDups[j])
-            {
-                for (int k = j; k < codepointsNoDupsCount; k++) codepointsNoDups[k] = codepointsNoDups[k + 1];
-
-                codepointsNoDupsCount--;
-                j--;
-            }
-        }
-    }
-
-    // NOTE: The size of codepointsNoDups is the same as original array but
-    // only required positions are filled (codepointsNoDupsCount)
-
-    *codepointsResultCount = codepointsNoDupsCount;
-    return codepointsNoDups;
-}
-
 int main(void) {
   AppState state             = {0};
   state.state                = WAITING_FOR_FILE;
--- src/memo.c
+++ src/memo.c
@@ -18,6 +18,33 @@ void handle_error(const char *msg) {
     exit(EXIT_FAILURE);
 }
 
+int *CodepointRemoveDuplicates(int *codepoints, int codepointCount, int *codepointsResultCount) {
+    int codepointsNoDupsCount = codepointCount;
+    int *codepointsNoDups = (int *)calloc(codepointCount, sizeof(int));
+    memcpy(codepointsNoDups, codepoints, codepointCount*sizeof(int));
+
+    // Remove duplicates
+    for (int i = 0; i < codepointsNoDupsCount; i++)
+    {
+        for (int j = i + 1; j < codepointsNoDupsCount; j++)
+        {
+            if (codepointsNoDups[i] == codepointsNoDups[j])
+            {
+                for (int k = j; k < codepointsNoDupsCount; k++) codepointsNoDups[k] = codepointsNoDups[k + 1];
+
+                codepointsNoDupsCount--;
+                j--;
+            }
+        }
+    }
+
+    // NOTE: The size of codepointsNoDups is the same as original array but
+    // only required positions are filled (codepointsNoDupsCount)
+
+    *codepointsResultCount = codepointsNoDupsCount;
+    return codepointsNoDups;
+}
+
 bool is_utf8(const char * string) {
     if(!string)
         return 0;
